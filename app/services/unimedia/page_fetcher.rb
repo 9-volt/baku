3e6c799d6874bd3821b20cb3d12e1a9194d100d1
Ivan Zarea <zarea.ion@gmail.com>
2013-11-11 22:53:51 +0200

    Added the unimedia PageFetcher

--- app/services/unimedia/page_fetcher.rb
+++ app/services/unimedia/page_fetcher.rb
@@ -1,5 +1,24 @@
 class Unimedia::PageFetcher
-  def fetch
-    
+  attr_reader :link
+
+  def initialize(link_id)
+    @link = Link.find(link_id)
+  end
+
+  def fetch!
+    page_data = RestClient.get(link.url)
+
+    link.update_attributes!(attempted: true)
+
+    if valid? page_data
+      Page.create!(link: link, content: page_data)
+      link.update_attributes!(success: true)
+    else
+      link.update_attributes!(success: false)
+    end
+  end
+private
+  def valid?(page_data)
+    /Ne pare rău, pagina nu a fost găsită./ !~ page_data
   end
 end
---
+++ spec/services/unimedia/page_fetcher_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe Unimedia::PageFetcher do
+  let!(:link)        { FactoryGirl.create(:link, source: :unimedia) }
+
+  subject { Unimedia::PageFetcher.new(link.id) }
+
+  before do
+    RestClient.stub(:get).with(link.url).and_return(page_content)
+  end
+
+  context "when the page is valid" do
+  let(:page_content) { PageFixtures.load('unimedia_page_68152.html') }
+    it 'fetches the page from the URL and marks the link' do
+      subject.fetch!
+      Page.count.should == 1
+      Page.last.link.should == link
+      Page.last.content.should == page_content
+      link.reload.attempted.should == true
+      link.reload.success.should == true
+    end
+  end
+
+  context "with a 404 page" do
+    let(:page_content) { PageFixtures.load('unimedia_404.html') }
+    it 'does not save the page' do
+      subject.fetch!
+      Page.count.should == 0
+      link.reload.attempted.should == true
+      link.success.should == false
+    end
+  end
+end
