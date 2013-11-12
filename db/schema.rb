1aa9dd3f139c23ebf0d45e7b287807d7197e0a3e
Ivan Zarea <zarea.ion@gmail.com>
2013-11-12 23:37:54 +0200

    Removed keep and added schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131027193608) do
+ActiveRecord::Schema.define(version: 20131112202044) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "links", force: true do |t|
-    t.string   "url"
-    t.string   "source"
+    t.string   "url",                         null: false
+    t.string   "news_source"
     t.datetime "parsed_at"
-    t.boolean  "attempted"
-    t.boolean  "success"
+    t.boolean  "attempted",   default: false
+    t.boolean  "success",     default: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -32,5 +32,20 @@ ActiveRecord::Schema.define(version: 20131027193608) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "users", force: true do |t|
+    t.string   "email",               default: "", null: false
+    t.string   "encrypted_password",  default: "", null: false
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",       default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
