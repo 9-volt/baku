7ad2c1767e93c7067f08e3add1ee8e83c0611043
Ivan Zarea <zarea.ion@gmail.com>
2013-11-20 00:35:30 +0200

    Removed registrable from devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Baku::Application.routes.draw do
   require 'sidekiq/web'
 
-  devise_for :users
+  devise_for :users, skip: :registrations
   authenticate :user do
     root to: 'dashboard#index'
     mount Sidekiq::Web, at: '/sidekiq'
