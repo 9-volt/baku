464f157a96dc8b26ccb5d4a0b98c462ada7b21a5
Ivan Zarea <zarea.ion@gmail.com>
2013-11-19 22:44:57 +0200

    Added the megacard to dashboard

--- app/presenters/dashboard_presenter.rb
+++ app/presenters/dashboard_presenter.rb
@@ -1,7 +1,8 @@
 class DashboardPresenter
-  attr_reader :users
+  attr_reader :users, :updated_today
 
   def initialize
     @users = User.all
+    @updated_today = Link.recently_updated.from_source(:unimedia).successful.count
   end
 end
