ffe5b1d9a24e988d3a817fb602c513987e6efc8f
Andrei Lisnic <andrei.lisnic@gmail.com>
2013-11-24 16:26:58 +0200

    Improve protv link extraction

--- app/services/protv/link_generator.rb
+++ app/services/protv/link_generator.rb
@@ -9,9 +9,19 @@ class Protv::LinkGenerator < FbGraphLinkGenerator
     :protv
   end
 
+  def extract_url hash
+    logger.info "extract url from #{hash['message']}"
+    begin
+      URI.extract(hash["message"]).last || hash["link"]
+    rescue
+      hash["link"]
+    end
+  end
+
   def valid? link
     return false unless link.start_with? "http"
     return true if link.start_with? "http://go.protv.md"
+    return true if link.start_with? "http://bit.ly"
     false
   end
 end
