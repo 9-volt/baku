a01224ebdf06d2880e741180bc26426ee3269c7a
Andrei Lisnic <andrei.lisnic@gmail.com>
2013-11-27 21:33:00 +0200

    Fix protv parser, add Inprofunzime parser

--- app/services/inprofunzime/parser.rb
+++ app/services/inprofunzime/parser.rb
@@ -1,2 +1,38 @@
 class Inprofunzime::Parser < Protv::Parser
+  def parse_time
+    convert_time doc.css('.boxStire .boxTime').children.first.text
+  end
+
+  def parse_category
+    URI(url).path.split('/')[2]
+  end
+
+  def parse_author
+    doc.css('.articleSource').text
+  end
+
+  def parse_sentences
+    doc.css('.continut').children.map(&:text).map {|s| s.split(/[\.!?]/)}
+    .flatten.map(&:strip).select {|s| s.size > 1 }
+  end
+
+  def convert_time str
+    match_data = str.match(/(?<hours>\d+):(?<minutes>\d+), (?<day>\d+) (?<month>\S+) (?<year>\d+)/)
+    Time.new(match_data[:year],
+     convert_month(match_data[:month][0..2].downcase),
+     match_data[:day],
+     match_data[:hours],
+     match_data[:minutes])
+  end
+
+  def convert_month(str)
+    case str
+    when 'ian' then 'jan'
+    when 'mai' then 'may'
+    when 'iun' then 'jun'
+    when 'iul' then 'jul'
+    when 'noi' then 'nov'
+    else str
+    end
+  end
 end
--- app/services/protv/parser.rb
+++ app/services/protv/parser.rb
@@ -1,42 +1,22 @@
 class Protv::Parser < Parser
   def parse_time
-    convert_time doc.css('.boxStire .boxTime').children.first.text
-  end
-
-  def parse_author
-    doc.css('.articleSource').text
+    Time.parse doc.css('div[itemprop=datePublished]').first[:content]
   end
 
   def parse_category
-    ''#@url.split('/')[1]
+    doc.css('ul.breadcrumbs li').last.text.downcase.squish
+  end
+
+  def parse_author
+    doc.css('.article_source_url').first.text
   end
 
   def parse_sentences
-    doc.css('.continut').children.map(&:text).map {|s| s.split(/[\.!?]/)}
+    doc.css('.articleContent p').children.map(&:text).map {|s| s.split(/[\.!?]/)}
     .flatten.map(&:strip).select {|s| s.size > 1 }
   end
 
   def parse_title
     doc.css('title').text
-  end
-
-  def convert_time str
-    match_data = str.match(/(?<hours>\d+):(?<minutes>\d+), (?<day>\d+) (?<month>\S+) (?<year>\d+)/)
-    Time.new(match_data[:year],
-     convert_month(match_data[:month][0..2].downcase),
-     match_data[:day],
-     match_data[:hours],
-     match_data[:minutes])
-  end
-
-  def convert_month(str)
-    case str
-    when 'ian' then 'jan'
-    when 'mai' then 'may'
-    when 'iun' then 'jun'
-    when 'iul' then 'jul'
-    when 'noi' then 'nov'
-    else str
-    end
   end
 end
